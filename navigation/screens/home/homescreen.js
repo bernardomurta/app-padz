66ab918b286e79b18c0efce328221b99e63fb7e7
Bernardo Murta <bkatiaia@yahoo.com>
2021-11-18 11:22:46 -0300

    alteração

--- navigation/screens/home/homescreen.js
+++ navigation/screens/home/homescreen.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { View, Text } from 'react-native'
-import Option from '../../../components/options/options.js'
+import Option from '../../../components/foodoptions/options.js'
 export default function homescreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
